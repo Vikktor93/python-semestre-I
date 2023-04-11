7d7d57304308f8abb3a035c31254f41cf727b7f4
Vikktor93 <Vikktor93@users.noreply.github.com>
2023-04-11 19:59:05 -0400

    Se agrego información de Constantes

--- 01-variables.py
+++ 01-variables.py
@@ -1,9 +1,10 @@
+"""GUIA RÁPIDA: CONOCIENDO LAS VARIABLES EN PYTHON - 10 DE ABRIL 2023
+  Docente: Victor Saldivia Vera - Institución: Universidad de Los Lagos"""
+
 #ESTE UN COMENTARIO DE UNA LINEA
 """ ESTE 
 ES UN COMENTARIO 
 MULTILINEA """
-
-##GUIA RÁPIDA: CONOCIENDO LAS VARIABLES EN PYTHON - 10 DE ABRIL 2023
 
 #Un simple print
 print("Hola soy Constanza!")
@@ -22,13 +23,12 @@ edad = 29
 #03-IMPRESION DE TEXTO + VARIABLE (IMPRESIÓN VARIABLE EDAD)
 print("Mi edad es de", edad)
 
-
 #04-IMPRIMIENDO 2 VARIABLES EN UNA MISMA LINEA 
 #(concatenación con signo +, utilizando comas y Formato cadenas literales)
 #print("Hola mi nombre es" + nombre + "y tengo" + edad + "años") #Esta sentencia da error ¿por qué?
 
-print("Hola mi nombre es",nombre,"y tengo",edad) #Impresion separando con comas
-print("Hola mi nombre es"+" "+name+" "+"y tengo"+edad) #Concatenación con signo +
+print('Hola mi nombre es',nombre,"y tengo",edad) #Impresion separando con comas
+print("Hola mi nombre es"+" "+name+" "+"y tengo"+str(edad)) #Concatenación con signo +
 print(f"Hola mi nombre es {nombre} y tengo {edad} años") #Formato de cadenas literales (f-string)
 
 ##05-ACTUALIZANDO LA VARIABLE NOMBRE (Mutabilidad)
@@ -36,7 +36,6 @@ nombre = "Diego"
 name = "Benjamin"
 print ("Hola mi nuevo nombre es", nombre)
 print ("Hola mi nuevo nombre es", name)
-
 
 #06-¿VARIABLES EN UNA SOLA LINEA? (No es muy recomendable, solo en ciertas situaciones)
 ciudad, region, pais, year = "Puerto Montt", "Los Lagos", "Chile", 2010.
@@ -47,3 +46,9 @@ nombre1 = input("¿Cuál es tu nombre?\n")
 edad1 = input("¿Cual es tu edad?\n")
 
 print("Tu nombre es:",nombre1,"y tu edad es", edad1)
+
+#08-ACOTACIÓN CONSTANTES
+'''En Python no existen las constantes, por convención se identifican y se declaran con mayusculas
+como se muestra a continuación'''
+PI = 3.14
+CIUDAD = "Osorno"
