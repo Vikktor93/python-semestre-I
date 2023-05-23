2be1efbc168480fa64f1efdcada230a2c565f032
Vikktor93 <Vikktor93@users.noreply.github.com>
2023-05-23 11:21:19 -0400

    Actualización ejercicios condicionales

--- 04-condicionales.py
+++ 04-condicionales.py
@@ -1,54 +1,55 @@
 #GUIA RÁPIDA CONDICIONALES EN PYTHON
-# Docente: Victor Saldivia Vera - Institución: Universidad de Los Lagos
+#Docente: Victor Saldivia Vera - Institución: Universidad de Los Lagos
+
+from colorama import init, Fore
+init()
 
 #CONDICIONAL IF
-print('######## 01- UTILIZANDO IF y ELSE ########\n')
+print(Fore.GREEN + '################ 01 - UTILIZANDO IF y ELSE ################')
 
 licencia = False
 edad = 19
 automovil = False
 
-#¿Estará correcto este código?                                                       - Código Incorrecto  
-"""if licencia == True:
-    print('Puedo conducir porque tengo licencia')
+#¿Estará correcto este código?                                                  - Código Incorrecto  
+print(Fore.YELLOW +'\n>>> Testeando los comparadores y el IF')
+if licencia:
+    print(Fore.WHITE + 'Puedo conducir porque tengo licencia')
 else:
-    print('No tengo licencia para conducir')"""
-
-
-
+    print(Fore.WHITE + 'No tengo licencia para conducir')
 
 
 #Sucede que la variable licencia se esta comparando con True,
 #y se debe asignar directamente, es decir no se ocupa el ==, sino solamente el igual (=)
 
-print('######## Utilizando el primer condicional IF ########')
+print(Fore.YELLOW + '\n>>> Utilizando el primer condicional IF')
 if licencia:
-    print('Puedo conducir porque tengo licencia\n')
+    print(Fore.WHITE + 'Puedo conducir porque tengo licencia\n')
 else:
-    print('No tengo licencia para conducir\n')
-"""print("Este print fuera del else")"""
+    print(Fore.WHITE + 'No tengo licencia para conducir\n')
+print("Este print esta fuera del else")
 
 
-print('######## Utilizando el segundo condicional IF ########')
+print(Fore.YELLOW + '>>> Utilizando el segundo condicional IF')
 if edad:
-    print('Puedo conducir porque soy mayor de edad\n')
+    print(Fore.WHITE + 'Puedo conducir porque soy mayor de edad\n')
 else:
-    print('No puedo conducir soy menor de edad\n')
+    print(Fore.WHITE + 'No puedo conducir soy menor de edad\n')
     
 
-print('######## Utilizando el tercer condicional IF ########')
+print(Fore.YELLOW + '>>> Utilizando el tercer condicional IF')
 if edad >= 18:
-    print('Puedo conducir porque soy mayor de edad\n')
+    print(Fore.WHITE + 'Puedo conducir porque soy mayor de edad\n')
 else:
-    print('No puedo conducir soy menor de edad\n')
+    print(Fore.WHITE + 'No puedo conducir soy menor de edad\n')
 
 
-print('######## 02 UTILIZANDO IF y ELIF, ELSE ########\n')
+print(Fore.GREEN + '################ 02 - UTILIZANDO IF, ELIF y ELSE ################')
 if licencia and edad >= 18:
-    print('Puedo conducir porque soy mayor de edad y tengo licencia')
+    print(Fore.WHITE + 'Puedo conducir porque soy mayor de edad y tengo licencia')
 elif automovil:
-    print('Tengo automovil, pero no tengo licencia ni la edad necesaria')
+    print(Fore.WHITE + 'Tengo automovil, pero no tengo licencia ni la edad necesaria')
 else:
-    print('No puedo conducir no tengo ni la edad, ni licencia ni automovil')
+    print(Fore.WHITE + 'No puedo conducir no tengo ni la edad, ni licencia ni automovil')
 
 
