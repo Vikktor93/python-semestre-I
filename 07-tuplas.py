f25dfc56db1dcc330c0b01fdece62ef4ed70c544
Vikktor93 <Vikktor93@users.noreply.github.com>
2023-05-25 00:11:01 -0400

    Agregando el metodo next

--- 07-tuplas.py
+++ 07-tuplas.py
@@ -1,20 +1,19 @@
+#Declarando dos tuplas (también se puede hacer sin parentesis)
 t = 1, 2, 3
-tuplita = "Pedrito", "Tito"
+tuplita = "Pedrito", "Tito", "Andre", "Barry"
+
+#Se puede ordenar una tupla con Sorted (pero el resultado es una lista ordenada)
+tuplaordenada = sorted(tuplita)
+print(tuplaordenada)
 
 print(type(t))
 print(type(tuplita))
-
-
 
 alumnas = [("Lorena", 6.5, 6.9), ("Maria", 3.9, 4.1), ("Anais", 5.4, 4.8)]
 print(alumnas)
 
 for estudiante in alumnas:
     print(estudiante)
-
-
-
-
 
 promedios = []
 
---
+++ test.py
@@ -0,0 +1,16 @@
+edad = 66
+
+if edad >= 18 and edad < 65:
+    print('Eres mayor de edad')
+elif edad >= 65:
+    print('Eres un adulto mayor')
+else:
+    print('Eres menor de edad')
+
+
+x = 5
+print("Es 5" if x == 5 else "No es 5")
+#Es 5
+
+edad = 19
+print("Usted puede votar" if edad >= 18 else "Usted no puede votar")
