7c143d23c62ace76862a12efa576205e231081b5
Vikktor93 <Vikktor93@users.noreply.github.com>
2024-08-15 15:39:28 -0400

    Se actualiza el archivo de Clases de Python

--- 09-clases.py
+++ 09-clases.py
@@ -1,7 +1,27 @@
+# GUIA RÁPIDA DE CLASES Y OBJETOS EN PYTHON
+# Docente: Victor Saldivia Vera
+# Universidad de Los Lagos
 class Persona(): 
-    def __init__(self, nombre, apellido, edad): 
-        self.nombre = nombre 
-        self.apellido = apellido
-        self.edad = edad 
+        # Atributos de Clase (Características compartidas por todos los objetos de la clase)
+        nombre = "Cristina" 
+        apellido = "Torres"
+        edad = 23
         
-Persona1 = Persona(23, "Cristina", "Torres") 
+        # Métodos (Comportamientos)
+        def estudiar(self):
+            print(f"{self.nombre} esta estudiando")
+        
+        def dormir(self):
+            print(f"{self.nombre} esta durmiendo")
+            
+# Creación de un objeto de la clase Persona
+persona1 = Persona()
+
+# Acceso a los atributos y métodos del objeto
+print(f"Nombre: {persona1.nombre}")
+print(f"Apellido: {persona1.apellido}")
+print(f"Edad: {persona1.edad} años")
+
+# Llamando a los métodos de la Clase
+persona1.estudiar()
+persona1.dormir()
